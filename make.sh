c42cbad27d6d3f8091cbc7d0df611567c0d99359
a <jh7qbe@virginia.edu>
2021-04-04 18:30:04 -0400

    changed to newer pandoc stuff

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" ]]
 then
-	pandoc -s --katex=../katex/katex.min.js --katex-stylesheet=../katex/katex.min.css "$1.md" -o "$1.html"
+	pandoc -s --metadata title="$1" --katex=../katex/ "$1.md" -o "$1.html"
 else
 	echo "you forgot arg i think"
 fi
