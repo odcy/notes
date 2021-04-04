782dcc1986dadf8e799ca372004ed692fe51cc92
a <jh7qbe@virginia.edu>
2021-04-04 18:31:16 -0400

    oops

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" ]]
 then
-	pandoc -s --metadata title="$1" --katex=../katex/ "$1.md" -o "$1.html"
+	pandoc -s --metadata pagetitle="$1" --katex=../katex/ "$1.md" -o "$1.html"
 else
 	echo "you forgot arg i think"
 fi
